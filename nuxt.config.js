51ca7d5f18b3ffca87a4a334660e6dd31c9a9e30
ZhanVz <nath_sqz@outlook.com>
2020-07-28 22:17:15 -0500

    cambios en config

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,10 @@ export default {
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
   */
-  target: 'server',
+  target: 'static',
+  router: {
+    base: '/Tenis/'
+  },
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
